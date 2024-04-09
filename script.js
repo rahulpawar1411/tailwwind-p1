8225cbd3f25a86c84c62798e694303abdab61779
Rahul pawar <145052835+rahulpawar1411@users.noreply.github.com>
2024-04-09 16:40:27 +0530

    Add files via upload
    first

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let page1 = document.querySelector('#page1');
+// let login = document.querySelector('#login');
+let signup1 = document.querySelector('#signup1');
+// let signup2 = document.querySelector('#signup2');
+
+
+let page3 = document.querySelector('#page3');
+page3.style.display = "none"; 
+
+signup1.addEventListener("click", function(){
+    page1.style.display = "none";
+    page3.style.display = ""; 
+
+});
+
+
+
+
+
+
+
+
